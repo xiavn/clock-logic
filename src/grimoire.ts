a08381f5311a7655f961a2064551c991cb63d2bd
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-04-25 18:06:17 +0100

    set up test grimoire

---
+++ src/grimoire.ts
@@ -0,0 +1,73 @@
+const grimoire = {
+    playerCount: 15,
+    outsiders: 2,
+    minions: 3,
+    demon: 1,
+    townsfolk: 9,
+    bluffs: ["investigator", "soldier", "butler"],
+    edition: "tb",
+    townsquare: [
+        {
+            name: "Leah",
+            role: "drunk",
+        },
+        {
+            name: "Will",
+            role: "mayor",
+        },
+        {
+            name: "Justin",
+            role: "chef",
+        },
+        {
+            name: "Mart",
+            role: "slayer",
+        },
+        {
+            name: "Tom",
+            role: "ravenkeeper",
+        },
+        {
+            name: "Dave",
+            role: "fortuneteller",
+        },
+        {
+            name: "Debs",
+            role: "washerwoman",
+        },
+        {
+            name: "Bertie",
+            role: "saint",
+        },
+        {
+            name: "Kyle",
+            role: "empath",
+        },
+        {
+            name: "Lianne",
+            role: "poisoner",
+        },
+        {
+            name: "Ryan",
+            role: "monk",
+        },
+        {
+            name: "Alex",
+            role: "scarletwoman",
+        },
+        {
+            name: "Arif",
+            role: "imp",
+        },
+        {
+            name: "Jamie",
+            role: "librarian",
+        },
+        {
+            name: "Jams",
+            role: "spy",
+        },
+    ],
+};
+
+export default grimoire;
